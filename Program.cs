d88e61bb03e9bc08c50716b3f4df7c9cb0631e64
Ekaterina <92665364+Limushkina@users.noreply.github.com>
2022-01-03 22:06:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+int[] CreateArray(int count)
+{
+    int[] array = new int[count];
+    for (int i = 0; i < count; i++) 
+    {
+        array[i] = new Random().Next();
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{array[i]}");
+    }
+}
+
+int[] NewArray(int[] array)
+{
+    int[] array2 = new int[array.Length];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            array2[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref array2, count);
+    return array2;
+}
+PrintArray(CreateArray(1));
+PrintArray(NewArray(1));
+
